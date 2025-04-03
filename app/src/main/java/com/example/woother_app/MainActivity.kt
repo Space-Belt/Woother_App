66b680d8ee2ba5e39e70022b1939d13847603fd6
Space-Belt <dngurwn@gmail.com>
2025-04-03 17:17:06 +0900

    feat: add imports

--- app/src/main/java/com/example/woother_app/MainActivity.kt
+++ app/src/main/java/com/example/woother_app/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.woother_app
 
 import android.os.Bundle
+import android.os.*
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+// api
+import org.json.JSONObject
+// 입력 스트림 / html / url 연결 / api /
+import java.io.BufferedReader
+// 입력
+import java.io.InputStream
+import java.io.InputStreamReader
+// api 연결설정
+import java.net.HttpURLConnection
+// url 읽기
+import java.net.URL
+
+import java.util.concurrent.ExecutionException
+
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
