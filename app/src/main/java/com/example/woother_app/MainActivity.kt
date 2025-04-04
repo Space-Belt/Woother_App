5cabca83634a014bd9f9cba8f890e0bb1d1dc32f
Space-Belt <dngurwn@gmail.com>
2025-04-04 20:18:08 +0900

    feat: xml 추가 요소 찾기 및 api 요청 추가

--- app/src/main/java/com/example/woother_app/MainActivity.kt
+++ app/src/main/java/com/example/woother_app/MainActivity.kt
@@ -31,10 +31,38 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var cityName: TextView
+    private lateinit var search: Button
+    private lateinit var show: TextView
+    private lateinit var url: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        cityName = findViewById(R.id.cityName)
+        search = findViewById(R.id.search)
+        show = findViewById(R.id.weather)
+
+        val temp = arrayOf("")
+
+        search.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button Clicked! ", Toast.LENGTH_SHORT).show()
+            val city = cityName.text.toString()
+            try {
+                if (city.isNotEmpty()) {
+                    url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid={API_KEY}"
+                } else {
+                    Toast.makeText(this@MainActivity, "Enter City", Toast.LENGTH_SHORT).show()
+                }
+                val task = GetWeather()
+                temp[0] = task.execute(url).get()
+            } catch (e: ExecutionException) {
+                e.printStackTrace()
+            }
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+apply<HelloWorldPlugin>()
+
+class HelloWorldPlugin: Plugin<Project> {
+    override fun apply(targe: Project) {
+        println("Hello Anroid")
+    }
+}
